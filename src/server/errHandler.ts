0d1e39dea256ff92073a0df17e8fd138a9fdc688
Edwin Ortiz <82191164+edarcode@users.noreply.github.com>
2024-09-28 20:59:57 -0500

    Initial commit

---
+++ src/server/errHandler.ts
@@ -0,0 +1,16 @@
+import { ZodError } from "zod";
+import { EdarErr } from "../errors/EdarErr";
+import { ErrHandler } from "../types";
+
+export const errorHandler: ErrHandler = (error, _req, res, _next) => {
+  if (error instanceof EdarErr) {
+    const { status, msg } = error;
+    return res.status(status).json({ msg });
+  }
+
+  if (error instanceof ZodError) {
+    return res.status(400).json(error);
+  }
+
+  return res.status(500).json(error);
+};
