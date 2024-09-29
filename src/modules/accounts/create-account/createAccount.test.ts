225a6d0a106b270a1024d1705fa828f5adbcc5aa
Edwin Ortiz <edarcode@gmail.com>
2024-09-29 00:20:51 -0500

    agg test

--- src/modules/accounts/create-account/createAccount.test.ts
+++ src/modules/accounts/create-account/createAccount.test.ts
@@ -1,22 +1,32 @@
 import req from "supertest";
 import server from "../../../server/server";
 
+const ROUTE = "/accounts/create-account";
+
 describe("POST /accounts/create-account", () => {
   it("Debería responder con status 400 si no envio email y password", async () => {
     const newUser = {};
-    const res = await req(server)
-      .post("/accounts/create-account")
-      .send(newUser);
+    const res = await req(server).post(ROUTE).send(newUser);
     expect(res.statusCode).toBe(400);
   });
 
-  // it("Debería crear un nuevo usuario si los datos son válidos", async () => {
-  //   const newUser = {
-  //     email: "doe@example.com",
-  //     password: "pass123",
-  //   };
+  it("Debería fallar si el email no tiene el formato correcto", async () => {
+    const res = await req(server).post(ROUTE).send({
+      email: "invalid-email", // email mal formado
+      password: "ValidPassword123!",
+    });
 
-  //   const res = await req(server).post("/users/create-user").send(newUser);
-  //   expect(res.statusCode).toBe(201);
-  // });
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toHaveProperty("issues"); // Esperamos que devuelva un error de Zod
+  });
+
+  it("Debería fallar si el password no tiene el formato correcto", async () => {
+    const res = await req(server).post(ROUTE).send({
+      email: "test@gmail.com",
+      password: "1", // password mal formado
+    });
+
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toHaveProperty("issues"); // Esperamos que devuelva un error de Zod
+  });
 });
