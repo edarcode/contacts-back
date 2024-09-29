dd1976dab39a258dfeafc31d92847f9f71a7f5ed
Edwin Ortiz <edarcode@gmail.com>
2024-09-29 06:43:38 -0500

    agg seed

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,1 +1,41 @@
+import "../services/dotenv";
+import bcrypt from "bcrypt";
+import { accounts, contacts, Role } from "./schemas";
+import { BCRYPT } from "../config/bcrypt";
+import { db } from "./db";
 
+const EDAR = {
+  id: crypto.randomUUID(),
+  email: process.env.ADMIN_EMAIL as string,
+  password: process.env.ADMIN_PASSWORD as string,
+  role: Role.admin,
+};
+
+const LORE = {
+  id: crypto.randomUUID(),
+  email: "lore@gmail.com",
+  password: "123456",
+};
+
+const MYKE = {
+  id: crypto.randomUUID(),
+  email: "myke@gmail.com",
+  password: "123456",
+};
+
+const seed = async () => {
+  EDAR.password = await bcrypt.hash(EDAR.password, BCRYPT.salt);
+  LORE.password = await bcrypt.hash(LORE.password, BCRYPT.salt);
+  MYKE.password = await bcrypt.hash(MYKE.password, BCRYPT.salt);
+
+  await db.delete(accounts).execute();
+  await db.insert(accounts).values([EDAR, LORE, MYKE]);
+
+  await db.insert(contacts).values([
+    { name: "lore", tell: "+57 302", accountId: EDAR.id },
+    { name: "edar", tell: "+57 303", accountId: LORE.id },
+    { name: "edar", tell: "+57 303", accountId: MYKE.id },
+  ]);
+};
+
+seed().catch(console.error);
