afaa495456b858f610425837246510417e40d201
Edwin Ortiz <edarcode@gmail.com>
2024-09-29 22:14:05 -0500

    lanzo 404 si intentar borrar cuenta que no existe

--- src/modules/accounts/remove-account/removeAccountController.ts
+++ src/modules/accounts/remove-account/removeAccountController.ts
@@ -6,6 +6,7 @@ export const removeAccountController: Controller = async (_req, res, next) => {
   try {
     const { id } = res.locals?.tokenPayload as TokenPayload;
     await removeAccountService(id);
+
     res.status(200).json({ msg: "Cuenta borrada correctamente" });
   } catch (error) {
     next(error);
--- src/modules/accounts/remove-account/removeAccountService.ts
+++ src/modules/accounts/remove-account/removeAccountService.ts
@@ -2,7 +2,11 @@ import { eq } from "drizzle-orm";
 import { db } from "../../../db/db";
 import { UUID } from "crypto";
 import { accounts } from "../../../db/schemas";
+import { EdarErr } from "../../../errors/EdarErr";
 
 export const removeAccountService = async (id: UUID) => {
-  await db.delete(accounts).where(eq(accounts.id, id));
+  const result = await db.delete(accounts).where(eq(accounts.id, id));
+
+  if (!result.rowsAffected)
+    throw new EdarErr({ status: 404, msg: "Registro no encontrado" });
 };
