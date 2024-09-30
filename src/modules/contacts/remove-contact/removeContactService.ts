0deaea185db4c15cbafcd058b1e41b4ef2617b9c
Edwin Ortiz <edarcode@gmail.com>
2024-09-29 22:07:54 -0500

    lanzo msg de err si no encuentro contacto a eliminar

--- src/modules/contacts/remove-contact/removeContactService.ts
+++ src/modules/contacts/remove-contact/removeContactService.ts
@@ -2,12 +2,16 @@ import { and, eq } from "drizzle-orm";
 import { db } from "../../../db/db";
 import { UUID } from "crypto";
 import { contacts } from "../../../db/schemas";
+import { EdarErr } from "../../../errors/EdarErr";
 
 export const removeContactService = async (
   accountId: UUID,
   contactId: UUID
 ) => {
-  await db
+  const result = await db
     .delete(contacts)
     .where(and(eq(contacts.accountId, accountId), eq(contacts.id, contactId)));
+
+  if (!result.rowsAffected)
+    throw new EdarErr({ status: 404, msg: "Registro no encontrado" });
 };
